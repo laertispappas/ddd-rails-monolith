b40087254a78e2f4fb85a1ae4b61666876416ef2
Laerti Papa <laerti.papa@xing.com>
2020-01-25 03:19:57 +0100

    Moves application controller to shared model

---
+++ app/bounded_contexts/booking/application/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+module Booking
+  module Application
+    module Controllers
+      class ApplicationController < SharedDomain::Application::Controllers::ApplicationController
+        # Booking specific top level app controller logic
+      end
+    end
+  end
+end
---
+++ app/bounded_contexts/shared_domain/application/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+module SharedDomain
+  module Application
+    module Controllers
+      class ApplicationController < ActionController::API
+      end
+    end
+  end
+end
+
