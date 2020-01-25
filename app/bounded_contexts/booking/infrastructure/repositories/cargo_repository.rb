a7e0b661f54e52a657fe329c81448b900904aa45
Laerti Papa <laerti.papa@xing.com>
2020-01-25 18:16:25 +0100

    Adds cargo repo empty methods

--- app/bounded_contexts/booking/infrastructure/repositories/cargo_repository.rb
+++ app/bounded_contexts/booking/infrastructure/repositories/cargo_repository.rb
@@ -2,6 +2,23 @@ module Booking
   module Infrastructure
     module Repositories
       class CargoRepository
+
+        def store(cargo)
+          # (entity_manage | unit_of_work).persist(cargo) ?
+          # (entity_manage | unit_of_work).commit ?
+        end
+
+        def find_all
+          # (entity_manage | unit_of_work).find_all(query)
+        end
+
+        def find_all_booking_ids
+          # TODO
+        end
+
+        def next_booking_id
+          SecureRandom.uuid.upcase
+        end
       end
     end
   end
