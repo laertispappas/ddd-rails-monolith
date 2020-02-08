c0033814a6420d644e6046227827acdb86c4a5ad
Laerti Papa <laerti.papa@xing.com>
2020-02-08 21:53:48 +0100

    Fixes inject reference for tracking bc

--- app/bounded_contexts/tracking/infrastructure/repositories/tracking_repository.rb
+++ app/bounded_contexts/tracking/infrastructure/repositories/tracking_repository.rb
@@ -2,7 +2,7 @@ module Tracking
   module Infrastructure
     module Repositories
       class TrackingRepository < ActiveRepository::BaseRepository
-        include Booking::AppContainer::Inject[tracking_oersistence: "tracking.tracking_persistence"]
+        include Tracking::Container::Inject[tracking_oersistence: "tracking.tracking_persistence"]
       end
     end
   end
