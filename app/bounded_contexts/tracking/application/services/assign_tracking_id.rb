3ec58037272e2be117a64e51355b2e5288198329
Laerti Papa <laerti.papa@xing.com>
2020-02-08 21:57:51 +0100

    Adds tracking event command

--- app/bounded_contexts/tracking/application/services/assign_tracking_id.rb
+++ app/bounded_contexts/tracking/application/services/assign_tracking_id.rb
@@ -2,6 +2,13 @@ module Tracking
   module Application
     module Services
       class AssignTrackingId
+        include Container::Inject[tracking_repo: "tracking.tracking_repository"]
+
+        def assign_tracking_number_to_cargo(assign_tracking_number_command)
+        end
+
+        def add_tracking_event(add_tracking_event_command)
+        end
       end
     end
   end
---
+++ app/bounded_contexts/tracking/domain/commands/add_tracking_event.rb
@@ -0,0 +1,13 @@
+module Tracking
+  module Domain
+    module Commands
+      class AssignTrackingNumber < Dry::Struct
+        attribute :booking_id, Types::String
+        attribute :event_time, Types::DateTime
+        attribute :event_type, Types::String
+        attribute :location, Types::String
+        attribute :voyage_number, Types::String
+      end
+    end
+  end
+end
