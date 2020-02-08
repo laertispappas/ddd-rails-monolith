1c116233d246b3c662825ff0f132198dde9a0613
Laerti Papa <laerti.papa@xing.com>
2020-02-08 22:23:52 +0100

    impl TA.from_command

--- app/bounded_contexts/tracking/domain/aggregates/tracking_activity.rb
+++ app/bounded_contexts/tracking/domain/aggregates/tracking_activity.rb
@@ -3,8 +3,16 @@ module Tracking
     module Aggregates
       class TrackingActivity < Entity
         attribute :tracking_number, Types.Instance(Aggregates::TrackingNumber)
-        attribute :tracking_booking_id, Types.Instance(Entities::TrackingBookingId)
+        attribute :booking_id, Types.Instance(Entities::TrackingBookingId)
         attribute :tracking_activity_event, Types.Instance(Entities::TrackingActivityEvent)
+
+        def self.from_command(assign_tracking_number_command)
+          new(
+            tracking_number: TrackingNumber.new(value: assign_tracking_number_command.tracking_number),
+            booking_id: TrackingBookingId.new(value: assign_tracking_number_command.booking_id),
+            trackingActivityEvent: TrackingActivityEvent.EMPTY_ACTIVITY
+          )
+        end
       end
     end
   end
