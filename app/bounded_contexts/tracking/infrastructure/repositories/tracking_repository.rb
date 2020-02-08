cca5774d2e01129ad2513dcfa2748eb1877ae698
Laerti Papa <laerti.papa@xing.com>
2020-02-08 21:56:29 +0100

    Adds tracking repo method signatures

--- app/bounded_contexts/tracking/infrastructure/repositories/tracking_repository.rb
+++ app/bounded_contexts/tracking/infrastructure/repositories/tracking_repository.rb
@@ -3,6 +3,13 @@ module Tracking
     module Repositories
       class TrackingRepository < ActiveRepository::BaseRepository
         include Tracking::Container::Inject[tracking_oersistence: "tracking.tracking_persistence"]
+
+        def assign_tracking_number_to_cargo(assign_tracking_number_command)
+        end
+
+        def add_tracking_event(add_tracking_event_command)
+          
+        end
       end
     end
   end
---
+++ spec/bounded_contexts/tracking/infrastructure/repositories/tracking_repository_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+module Tracking
+  module Infrastructure
+    module Repositories
+      describe TrackingRepository, type: :model do
+        let(:repo) { TrackingRepository.new }
+
+      end
+    end
+  end
+end
