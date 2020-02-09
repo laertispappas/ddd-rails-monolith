ff21bc5f3b54146a3480115c9b114fff65b08ea9
Laerti Papa <laerti.papa@xing.com>
2020-02-09 01:13:55 +0100

    Register services in routing container

--- app/bounded_contexts/routing/container.rb
+++ app/bounded_contexts/routing/container.rb
@@ -1,6 +1,17 @@
 module Routing
   class Container < SharedDomain::Container
     namespace :routing do
+      register(:voyage_repository) {
+        Infrastructure::Repositories::VoyageRepository.new
+      }
+
+      register(:voyage_persistence) {
+        Infrastructure::Persistence::VoyagePersistence
+      }
+
+      register(:carrier_movement_persistence) {
+        Infrastructure::Persistence::CarrierMovementPersistence
+      }
     end
 
     Inject = Dry::AutoInject(Container)
