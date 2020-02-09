883256381996471d666fe164730c1222bf67a044
Laerti Papa <laerti.papa@xing.com>
2020-02-09 21:51:09 +0100

    Adds routing routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   end
   post "/bookings", to: UrlHelper.path(:booking, "cargo_bookings", :create)
   post "/cargorouting", to: UrlHelper.path(:booking, "cargo_routing", :create)
+  get "/optimal_route", to: UrlHelper.path(:routing, "cargo_routing", :find_optimal_route)
 end
