6107ed21c1402e8b1236b7019976838c956d6fd2
Laerti Papa <laerti.papa@xing.com>
2020-02-09 21:52:52 +0100

    Adds cargo routing query service

--- app/bounded_contexts/routing/application/queries/cargo_routing_query.rb
+++ app/bounded_contexts/routing/application/queries/cargo_routing_query.rb
@@ -2,6 +2,13 @@ module Routing
   module Application
     module Queries
       class CargoRoutingQuery
+        include Routing::Container::Inject[
+          voyage_repository: "routing.voyage_repository"
+        ]
+
+        def find_all
+          voyage_repository.find_all
+        end
       end
     end
   end
